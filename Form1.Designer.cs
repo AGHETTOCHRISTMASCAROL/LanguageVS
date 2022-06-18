f771e4680f80df0eff8c972ed2c2bf67100c4817
AGHETTOCHRISTMASCAROL <ivansavrikow@gmail.com>
2022-06-18 22:35:24 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace LanguageVS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.tbFIO = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnMinimized = new System.Windows.Forms.Button();
+            this.tbPhone = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Menu;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.WindowFrame;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.GridColor = System.Drawing.Color.Silver;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 128);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Brown;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1020, 354);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.White;
+            this.btnDelete.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnDelete.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnDelete.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDelete.Location = new System.Drawing.Point(900, 490);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 40);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // tbFIO
+            // 
+            this.tbFIO.BackColor = System.Drawing.Color.Gainsboro;
+            this.tbFIO.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbFIO.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbFIO.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.tbFIO.Location = new System.Drawing.Point(40, 30);
+            this.tbFIO.MaxLength = 120;
+            this.tbFIO.Name = "tbFIO";
+            this.tbFIO.Size = new System.Drawing.Size(310, 20);
+            this.tbFIO.TabIndex = 3;
+            this.tbFIO.Text = "ПОИСК ПО ФИО";
+            this.tbFIO.TextChanged += new System.EventHandler(this.TbFIO_TextChanged);
+            this.tbFIO.Enter += new System.EventHandler(this.tbFIO_Enter);
+            this.tbFIO.Leave += new System.EventHandler(this.tbFIO_Leave);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClose.ForeColor = System.Drawing.Color.Silver;
+            this.btnClose.Location = new System.Drawing.Point(1000, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(20, 20);
+            this.btnClose.TabIndex = 13;
+            this.btnClose.Text = "x";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // btnMinimized
+            // 
+            this.btnMinimized.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnMinimized.FlatAppearance.BorderSize = 0;
+            this.btnMinimized.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimized.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnMinimized.ForeColor = System.Drawing.Color.Silver;
+            this.btnMinimized.Location = new System.Drawing.Point(980, 0);
+            this.btnMinimized.Name = "btnMinimized";
+            this.btnMinimized.Size = new System.Drawing.Size(20, 20);
+            this.btnMinimized.TabIndex = 14;
+            this.btnMinimized.Text = "—";
+            this.btnMinimized.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnMinimized.UseVisualStyleBackColor = false;
+            this.btnMinimized.Click += new System.EventHandler(this.BtnMinimized_Click);
+            // 
+            // tbPhone
+            // 
+            this.tbPhone.BackColor = System.Drawing.Color.Gainsboro;
+            this.tbPhone.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbPhone.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbPhone.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.tbPhone.Location = new System.Drawing.Point(40, 54);
+            this.tbPhone.MaxLength = 20;
+            this.tbPhone.Name = "tbPhone";
+            this.tbPhone.Size = new System.Drawing.Size(310, 20);
+            this.tbPhone.TabIndex = 15;
+            this.tbPhone.Text = "ПОИСК ПО НОМЕРУ";
+            this.tbPhone.TextChanged += new System.EventHandler(this.TbPhone_TextChanged);
+            this.tbPhone.Enter += new System.EventHandler(this.tbPhone_Enter);
+            this.tbPhone.Leave += new System.EventHandler(this.tbPhone_Leave);
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.BackColor = System.Drawing.Color.Gainsboro;
+            this.tbEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbEmail.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbEmail.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.tbEmail.Location = new System.Drawing.Point(40, 78);
+            this.tbEmail.MaxLength = 50;
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(310, 20);
+            this.tbEmail.TabIndex = 16;
+            this.tbEmail.Text = "ПОИСК ПО EMAIL";
+            this.tbEmail.TextChanged += new System.EventHandler(this.TbEmail_TextChanged);
+            this.tbEmail.Enter += new System.EventHandler(this.tbEmail_Enter);
+            this.tbEmail.Leave += new System.EventHandler(this.tbEmail_Leave);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.BackColor = System.Drawing.Color.Transparent;
+            this.BtnUpdate.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnUpdate.BackgroundImage")));
+            this.BtnUpdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnUpdate.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.BtnUpdate.FlatAppearance.BorderSize = 0;
+            this.BtnUpdate.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.BtnUpdate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.BtnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnUpdate.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnUpdate.Location = new System.Drawing.Point(854, 490);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(40, 40);
+            this.BtnUpdate.TabIndex = 17;
+            this.BtnUpdate.UseVisualStyleBackColor = false;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1020, 681);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbPhone);
+            this.Controls.Add(this.btnMinimized);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.tbFIO);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox tbFIO;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMinimized;
+        private System.Windows.Forms.TextBox tbPhone;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Button BtnUpdate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LanguageVS
+{
+    public partial class Form1 : Form
+    {
+        #region toolBAR
+
+        private void BtnMinimized_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e) // перетаскивание формы 
+        {
+            base.Capture = false;
+            Message m = Message.Create(base.Handle, 0xa1, new IntPtr(2), IntPtr.Zero);
+            this.WndProc(ref m);
+        }
+
+        #endregion
+
+        #region Объявление переменных
+        //static String connectionString = @"data source=10.111.105.2,1433\SQLEXPRESS;initial catalog=Language20101;user id=20.101-17;password=Siv3836;MultipleActiveResultSets=True;App=EntityFramework";
+        static String connectionString = "data source = CAROLMACHINE; initial catalog = LanguageLanguage; integrated security = True; MultipleActiveResultSets = True; App = EntityFramework";
+        SqlConnection connection = new SqlConnection(connectionString);
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+        String quary = "";
+
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.Clear();
+            connection.Open();
+            quary = "SELECT C.ID, C.LastName AS 'Фамилия', C.FirstName AS 'Имя', C.Patronymic AS 'Отчество', G.Name AS 'Пол', C.Phone AS 'Телефон', C.Email, C.Birthday AS 'Birthday', C.RegistrationDate AS 'Registered' " +
+                    "FROM Client AS C JOIN Gender AS G ON C.GenderCode = G.Code";
+
+            adapter = new SqlDataAdapter(quary, connection);
+
+            adapter.Fill(table);
+
+            dataGridView1.DataSource = table;
+
+            dataGridView1.ClearSelection();
+            dataGridView1.Columns[0].HeaderCell.Style.BackColor = Color.DarkGray;
+            dataGridView1.Columns[0].Width = 40;
+            dataGridView1.Columns[1].Width = 120;
+            dataGridView1.Columns[2].Width = 100;
+            dataGridView1.Columns[3].Width = 140;
+            dataGridView1.Columns[4].Width = 80;
+            dataGridView1.Columns[5].Width = 130;
+            dataGridView1.Columns[6].Width = 190;
+            dataGridView1.Columns[7].Width = 100;
+            dataGridView1.Columns[8].Width = 100;
+
+            connection.Close();
+        } //Вывод при запуске
+
+        private void TbFIO_TextChanged(object sender, EventArgs e)
+        {
+            table.Clear();
+            ArrayList arrayList = new ArrayList();
+            connection.Open();
+            arrayList.AddRange(tbFIO.Text.Split(' '));
+
+            for (int i = arrayList.Count; i < 3; i++)
+                arrayList.Add("%");
+
+            quary = $"SELECT C.ID, C.LastName AS 'Фамилия', C.FirstName AS 'Имя', C.Patronymic AS 'Отчество', G.Name AS 'Пол', C.Phone AS 'Телефон', C.Email, C.Birthday AS 'Birthday', C.RegistrationDate AS 'Registered' " +
+                    $"FROM Client AS C JOIN Gender AS G ON C.GenderCode = G.Code " +
+                    $"WHERE C.LastName like '{arrayList[0]}%' AND c.FirstName like '{arrayList[1]}%' AND C.Patronymic like '{arrayList[2]}%'";
+
+            adapter = new SqlDataAdapter(quary, connection);
+
+            adapter.Fill(table);
+
+            dataGridView1.DataSource = table;
+            connection.Close();
+        } //Поиск по ФИО в реальном времени
+
+        private void TbPhone_TextChanged(object sender, EventArgs e)
+        {
+            table.Clear();
+            connection.Open();
+
+            quary = "SELECT C.ID, C.LastName AS 'Фамилия', C.FirstName AS 'Имя', C.Patronymic AS 'Отчество', G.Name AS 'Пол', C.Phone AS 'Телефон', C.Email, C.Birthday AS 'Birthday', C.RegistrationDate AS 'Registered' " +
+                    "FROM Client AS C JOIN Gender AS G ON C.GenderCode = G.Code " +
+                    $"WHERE C.Phone like ' {tbPhone.Text}%'"; // пробел не забыть убрать
+
+            adapter = new SqlDataAdapter(quary, connection);
+
+            adapter.Fill(table);
+
+            dataGridView1.DataSource = table;
+            connection.Close();
+        } //Поиск по телефону
+
+        private void TbEmail_TextChanged(object sender, EventArgs e)
+        {
+            table.Clear();
+            connection.Open();
+
+            quary = $"SELECT C.ID, C.LastName AS 'Фамилия', C.FirstName AS 'Имя', C.Patronymic AS 'Отчество', G.Name AS 'Пол', C.Phone AS 'Телефон', C.Email, C.Birthday AS 'Birthday', C.RegistrationDate AS 'Registered' " +
+                    $"FROM Client AS C JOIN Gender AS G ON C.GenderCode = G.Code " +
+                    $"WHERE C.Email like ' {tbEmail.Text}%'"; // пробел не забыть убрать
+
+            adapter = new SqlDataAdapter(quary, connection);
+
+            adapter.Fill(table);
+
+            dataGridView1.DataSource = table;
+            connection.Close();
+        } //Поиск по email
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            int row = int.Parse(dataGridView1[0, dataGridView1.CurrentRow.Index].Value.ToString());
+            table.Clear();
+            connection.Open();
+
+            quary = $"DELETE FROM Client WHERE ID = '{row}' " +
+                    $"SELECT C.ID, C.LastName AS 'Фамилия', C.FirstName AS 'Имя', C.Patronymic AS 'Отчество', G.Name AS 'Пол', C.Phone AS 'Телефон', C.Email, C.Birthday AS 'Birthday', C.RegistrationDate AS 'Registered' " +
+                    $"FROM Client AS C JOIN Gender AS G ON C.GenderCode = G.Code";
+
+            adapter = new SqlDataAdapter(quary, connection);
+
+            adapter.Fill(table);
+
+            dataGridView1.DataSource = table;
+            connection.Close();
+
+            MessageBox.Show("Клиент удален");
+        } //удаление по выделению строки
+
+        #region Подсказки
+        private void tbFIO_Leave(object sender, EventArgs e)
+        {
+            tbFIO.Text = "ПОИСК ПО ФИО";
+        }
+
+        private void tbPhone_Leave(object sender, EventArgs e)
+        {
+            tbPhone.Text = "ПОИСК ПО НОМЕРУ";
+        }
+
+        private void tbEmail_Leave(object sender, EventArgs e)
+        {
+            tbEmail.Text = "ПОИСК ПО EMAIL";
+        }
+
+        private void tbFIO_Enter(object sender, EventArgs e)
+        {
+            if (tbFIO.Text == "ПОИСК ПО ФИО")
+                tbFIO.Clear();
+        }
+
+        private void tbPhone_Enter(object sender, EventArgs e)
+        {
+            if (tbPhone.Text == "ПОИСК ПО НОМЕРУ")
+                tbPhone.Clear();
+        }
+
+        private void tbEmail_Enter(object sender, EventArgs e)
+        {
+            if (tbEmail.Text == "ПОИСК ПО EMAIL")
+                tbEmail.Clear();
+        }
+        #endregion
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+        }
+    }
+}
